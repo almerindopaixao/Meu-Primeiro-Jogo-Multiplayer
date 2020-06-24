dbdabea0f006f250aed96726f93330df8bf36bf9
Almerindo Paixão <almerindo_paixao@live.com>
2020-06-24 01:29:18 -0300

    update

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const app = express();
-
+const http = require('http');
 const routes = require('./routes');
 const path = require('path');
+
+const app = express();
+const server = http.createServer(app)
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
@@ -11,5 +13,5 @@ app.use(express.static((path.resolve(__dirname, 'public'))));
 app.use(routes);
 
 app.listen(3333, () => {
-    console.log('Servidor executando na porta 3333')
+    console.log('> Server listerning on port: 3333')
 });
